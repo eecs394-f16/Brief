d07021dc8b5592c53414d068cd0a8d7746ff9bcf
Daniel Nussbaum <daniel.e.nussbaum@gmail.com>
2016-09-26 17:42:43 -0500

    news

--- app/brief/scripts/NewsController.js
+++ app/brief/scripts/NewsController.js
@@ -1,2 +1,21 @@
 angular.module('brief').controller('NewsController', ['$scope', function($scope, $http) {
+    $scope.news = [
+        {
+            "type": 4,
+            "data":{
+                "name":"Student Organizations & Activities will meet all student groups to discuss inclusivity, membership",
+                "img":"http://dailynorthwestern.com/wp-content/uploads/2016/09/unnamed-4-900x601.jpg",
+                "link": "http://dailynorthwestern.com/2016/09/22/campus/student-organizations-activities-will-meet-all-student-groups-to-discuss-inclusivity-membership/"
+            }
+        },
+        {
+            "type": 4,
+            "data":{
+                "name":"University hires architectural firm to begin planning Black House renovations",
+                "img":"",
+                "link": "http://dailynorthwestern.com/2016/09/22/campus/university-hires-architectural-firm-to-begin-planning-black-house-renovations/"
+            }
+        }
+    ];
+
 }]);
