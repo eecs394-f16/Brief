595354d53b4a7866776166f485497e0e45bab180
RZNewman <rudolfnewman2017@u.northwestern.edu>
2016-09-26 19:32:26 -0500

    finished controllers

--- app/brief/scripts/SportsController.js
+++ app/brief/scripts/SportsController.js
@@ -1,3 +1,35 @@
 angular.module('brief').controller('SportsController', ['$scope', function($scope) {
-    
+    $scope.sports= [
+    	{
+			"type": 0,
+			"data":{
+				"name":"Nebraska",
+				"sport": "Soccer",
+				"time":"7:00pm",
+				"date":"9/30",
+				"location":"Lakeside Field",
+				"img":"nebraska.png"
+			}
+		},
+		{
+			"type": 0,
+			"data":{
+				"name":"University of Illinois",
+				"sport": "Basketball",
+				"time":"7:00pm",
+				"date":"10/7",
+				"location":"Welsh-Ryan Arena",
+				"img":"UofI.png"
+			}
+		},
+		{
+			"type": 3,
+			"data":{
+				"name":"Nebraska drops Wildcats in Big Ten opener",
+				"sport": "Football",
+				"img":"nebraska.png",
+				"link": "http://dailynorthwestern.com/2016/09/25/sports/football-nebraska-drops-wildcats-in-big-ten-opener/"
+			}
+		}
+    ]
 }]);
--- app/brief/scripts/UpcomingController.js
+++ app/brief/scripts/UpcomingController.js
@@ -1,3 +1,35 @@
 angular.module('brief').controller('UpcomingController', ['$scope', function($scope) {
-
+	$scope.upcoming =
+	[
+	{
+		"type": 1,
+		"data":{
+			"name":"ASG applications due",
+			"time":"11:59pm",
+			"date": "10/4",
+			"img":"ASG.png"
+		}
+	}
+	{
+		"type": 1,
+		"data":{
+			"name":"Last day to drop classes",
+			"time":null,
+			"date": "10/25",
+			"img":"drop.png"
+		}
+	}
+	{
+		"type": 2,
+		"data":{
+			"name":"McCormick job fair",
+			"timeStart":"4:00pm",
+			"timeEnd": "7:00pm",
+			"dateStart": "10/2",
+			"dateEnd": null,
+			"location": "Tech",
+			"img":"eng.png"
+		}
+	}
+	]
 }]);
