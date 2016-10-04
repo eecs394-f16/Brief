b64aa86fc79ce0e34db73f893057e4b11b5209c9
RZNewman <rudolfnewman2017@u.northwestern.edu>
2016-10-03 20:20:04 -0500

    dummy data V1

--- app/brief/scripts/UpcomingController.js
+++ app/brief/scripts/UpcomingController.js
@@ -1,32 +1,57 @@
 angular.module('brief').controller('UpcomingController', ['$scope', function($scope) {
 	$scope.upcoming = [
-	{
-		"type": 1,
-		"data":{
-			"name":"ASG applications due",
-			"time":"11:59pm",
-			"date": "10/4",
-			"img":"/data/asg.png"
-		}
-	},
-	{
-		"type": 1,
-		"data":{
-			"name":"Last day to drop classes",
-			"time":"11:59pm",
-			"date": "10/25",
-			"img":"/data/drop.png"
-		}
-	},
+	
 	{
 		"type": 2,
 		"data":{
 			"name":"McCormick job fair",
 			"time":"4:00pm-7:00pm",
-			"date": "10/2",
+			"date": "10/3",
 			"location": "Tech",
 			"img":"/data/Eng.png"
 		}
 	}
+	,{
+		"type": 2,
+		"data":{
+			"name":"Building a Better Tomorrow",
+			"time":"5:30pm-7:30pm",
+			"date": "10/3",
+			"location": "The Garage",
+			"img":"/data/Eng.png"
+		}
+	}
+	,{
+		"type": 2,
+		"data":{
+			"name":"The Importance of Being Ernest",
+			"time":"6:30pm-8:00pm",
+			"date": "10/3",
+			"location": "Shanley Pavillion",
+			"img":"/data/Eng.png"
+		}
+	}
+	,{
+		"type": 2,
+		"data":{
+			"name":"NCE Concert",
+			"time":"7:30pm-9:30pm",
+			"date": "10/3",
+			"location": "Tech Auditorium",
+			"img":"/data/Eng.png"
+		}
+	}
+	,{
+		"type": 2,
+		"data":{
+			"name":"Eric Whitacre - Orchestral Conducting",
+			"time":"8:00pm-10:00pm",
+			"date": "10/3",
+			"location": "Cahn Auditorium",
+			"img":"/data/Eng.png"
+		}
+	}
+	
+	
 	];
 }]);
