c30678d2791a2ba15ac50c4135fc09f20f7d0149
Sonia Nigam <sonianigam@dhcp-10-105-178-243.wireless.northwestern.private>
2016-10-19 20:52:55 -0500

    weather.json

--- javascript/WeatherController.js
+++ javascript/WeatherController.js
@@ -1,23 +1,11 @@
 angular.module('brief').controller('WeatherController', ['$scope', function($scope, $http) {
-    $scope.weather = {
-  "main": {
-    "grnd_level": 1015.23,
-    "humidity": 92,
-    "pressure": 1015.23,
-    "sea_level": 1037.61,
-    "temp": 63,
-    "temp_max": 65,
-    "temp_min": 59
-  },
-  "weather": [
-    {
-      "description": "broken clouds",
-      "icon": "04n",
-      "id": 803,
-      "main": "Clouds"
-    }
-  ]
-}
+    $scope.weather =
 	$scope.class = "clouds-bg";
-
+	
+	$http({
+		method: 'GET',
+		url: '/weather.json'
+	}).then(function success(response){
+		$scope.weather = response;
+	});
 }]);
