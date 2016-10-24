be9fd5882380155bd376ecb9f7479edae5a9543a
Sonia Nigam <sonianigam@sbcglobal.net>
2016-10-24 13:33:11 -0500

    scss file

--- javascript/UpcomingController.js
+++ javascript/UpcomingController.js
@@ -8,6 +8,48 @@ angular.module('brief').controller('UpcomingController', ['$scope', '$http', fun
 		$scope.upcoming = response;
 		$scope.list_upcoming= $scope.upcoming.data.entries;
 		
+		var event;
+		for(var i=0;i<$scope.list_upcoming;i++){
+			event =$scope.list_upcoming[i];
+			switch (event.type){
+			case "Academic":
+				event.color = "Aca"
+				break;
+			case "Fitness & Recreation":
+				event.color = "Fit"
+				break;
+			case "Other":
+				event.color = "Oth"
+				break;
+			case "Religious":
+				event.color = "Rel"
+				break;
+			case "Lectures & Meetings":
+				event.color = "Lec"
+				break;
+			case "Athletics":
+				event.color = "Ath"
+				break;
+			case "Fine Arts":
+				event.color = "Fine"
+				break;
+			case "Social":
+				event.color = "Soc"
+				break;
+			case "Global & Civil Engagement":
+				event.color = "Glo"
+				break;
+			case "Training":
+				event.color = "Tra"
+				break;
+			case "Multicultural & Diversity":
+				event.color = "Mul"
+				break;
+				
+			}
+		}
+			
+		
 		
 	});
 	
